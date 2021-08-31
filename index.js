f5f41c4e2fa597c6c1b34a5b8e11e908da8b2602
Eric Yi <erickanginyi@gmail.com>
2021-08-31 11:50:58 -0400

    commit tree 092a60aac657095cfe3ebcc00af4db916ce13f92

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (array) => {
+    return array.slice(0, 2);
+}
+
+const returnLastTwoDrivers = (array) => {
+    return array.slice(array.length - 2, array.length);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (num) => {
+    return (fare) => num * fare;
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (array, func) => {
+    return func(array);
+}
